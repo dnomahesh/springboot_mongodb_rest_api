fae028731986345750259c5a30084ceea1733ce4
dnomahesh <maheshtripathijobs247@gmail.com>
2025-07-23 16:49:51 +0530

    Global Error Handling, duplicate entry fixed.

--- src/main/java/work/mywork/scm/spring_boot/entity/events/ContactEventHandler.java
+++ src/main/java/work/mywork/scm/spring_boot/entity/events/ContactEventHandler.java
@@ -9,22 +9,34 @@ import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
 import org.springframework.stereotype.Component;
 
 import work.mywork.scm.spring_boot.entity.Contact;
+import work.mywork.scm.spring_boot.entity.User;
+import work.mywork.scm.spring_boot.exception.DuplicateResourceException;
+import work.mywork.scm.spring_boot.repository.ContactRepository;
 
 @Component
 @RepositoryEventHandler(Contact.class)
-public class ContactEventHandler  {
+public class ContactEventHandler {
 
     private Logger log = LoggerFactory.getLogger(ContactEventHandler.class);
+    private ContactRepository contactRepository;
 
-    // @HandleBeforeSave
-    // public void handleBeforeSave(Contact contact) {
-    //     contact.setId(UUID.randomUUID().toString());
-    // }
+    ContactEventHandler(ContactRepository contactRepository) {
+        this.contactRepository = contactRepository;
+    }
 
     @HandleBeforeCreate
-    public void handleBeforeCreate(Contact contact){
+    public void handleBeforeCreate(Contact contact) {
+        if (contactRepository.existsByEmail(contact.getEmail())) {
+            throw new DuplicateResourceException("Email already exists.");
+        }
+
+        if (contactRepository.existsByPhoneNumber(contact.getPhoneNumber())) {
+            throw new DuplicateResourceException("Phone number already exists.");
+        }
+
         contact.setId(UUID.randomUUID().toString());
-        log.info("contact is being created. ->"+contact.getName());
+        log.info("contact is being created. ->" + contact.getName());
+
     }
 
 }
